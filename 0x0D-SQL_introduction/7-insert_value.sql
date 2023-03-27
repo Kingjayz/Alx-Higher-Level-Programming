8690a86a40044a916dabf99232bad82ac044f707
Kingjayz <tonimex2@gmail.com>
2023-03-27 16:12:45 -0700

    Update 0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Inserts a new row into the table first_table.
+-- Inserts a new row in the table first_table (current database).
+
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
